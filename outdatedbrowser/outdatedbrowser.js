89a34bc4a2ebc640ee1df2d5f44d77c0f33a523c
David Sherret <dsherret@gmail.com>
2015-02-04 23:43:48 -0500

    - Fixed inconsistent spacings and coding style - Fallback to default options instead of overwriting them - Made some global variables local - Use existing `outdated` variable in displayResponse instead of calling `.getElementById` again - Made more sense to have a single if statement that would check if it should quit rather than doing an if/else in this case - Removed `done` variable (probably left around from fadeOut)

--- outdatedbrowser/outdatedbrowser.js
+++ outdatedbrowser/outdatedbrowser.js
@@ -6,214 +6,194 @@ website:    http://www.burocratik.com
 * @preserve
 -----------------------------------------------------------------------*/
 var outdatedBrowser = function(options) {
+    var outdated = document.getElementById("outdated");
 
-    //Variable definition (before ajax)
-    var outdated = document.getElementById("outdated"),
-        test;
+    options = options || {};
 
-    // Default settings
-    this.defaultOpts = {
-        bgColor: '#f25648',
-        color: '#ffffff',
-        lowerThan: 'transform',
-        languagePath: '../outdatedbrowser/lang/en.html',
-        test: false
+    // default settings
+    var defaultOpts = {
+        bgColor      : '#f25648',
+        color        : '#ffffff',
+        lowerThan    : 'transform',
+        languagePath : '../outdatedbrowser/lang/en.html',
+        test         : false
+    };
+
+    // assign css3 property to IE browser version
+    if (options.lowerThan === 'IE8' || options.lowerThan === 'borderSpacing') {
+        options.lowerThan = 'borderSpacing';
+    } else if (options.lowerThan === 'IE9' || options.lowerThan === 'boxShadow') {
+        options.lowerThan = 'boxShadow';
+    } else if (options.lowerThan === 'IE10' || options.lowerThan === 'transform' || options.lowerThan === '' || typeof options.lowerThan === "undefined") {
+        options.lowerThan = 'transform';
+    } else if (options.lowerThan === 'IE11' || options.lowerThan === 'borderImage') {
+        options.lowerThan = 'borderImage';
     }
 
-    if (options) {
-        //assign css3 property to IE browser version
-        if(options.lowerThan == 'IE8' || options.lowerThan == 'borderSpacing') {
-            options.lowerThan = 'borderSpacing';
-        } else if (options.lowerThan == 'IE9' || options.lowerThan == 'boxShadow') {
-            options.lowerThan = 'boxShadow';
-        } else if (options.lowerThan == 'IE10' || options.lowerThan == 'transform' || options.lowerThan == '' || typeof options.lowerThan === "undefined") {
-            options.lowerThan = 'transform';
-        } else if (options.lowerThan == 'IE11' || options.lowerThan == 'borderImage') {
-            options.lowerThan = 'borderImage';
-        }
-        //all properties
-        this.defaultOpts.bgColor = options.bgColor;
-        this.defaultOpts.color = options.color;
-        this.defaultOpts.lowerThan = options.lowerThan;
-        this.defaultOpts.languagePath = options.languagePath;
-        this.defaultOpts.test = options.test;
+    options.bgColor       = options.bgColor      || defaultOpts.bgColor;
+    options.color         = options.color        || defaultOpts.color;
+    options.lowerThan     = options.lowerThan    || defaultOpts.lowerThan;
+    options.languagePath  = options.languagePath != null ? options.languagePath : defaultOpts.languagePath;
+    options.test          = options.test         || defaultOpts.test;
 
-        bkgColor = this.defaultOpts.bgColor;
-        txtColor = this.defaultOpts.color;
-        cssProp = this.defaultOpts.lowerThan;
-        languagePath = this.defaultOpts.languagePath;
-        test = this.defaultOpts.test;
-    } else {
-        bkgColor = this.defaultOpts.bgColor;
-        txtColor = this.defaultOpts.color;
-        cssProp = this.defaultOpts.lowerThan;
-        languagePath = this.defaultOpts.languagePath;
-        test = this.defaultOpts.test;
-    };//end if options
+    // define opacity and fadeIn functions
 
-
-    //Define opacity and fadeIn/fadeOut functions
-    var done = true;
-
-    function function_opacity(opacity_value) {
-        outdated.style.opacity = opacity_value / 100;
-        outdated.style.filter = 'alpha(opacity=' + opacity_value + ')';
+    function opacity(opacityValue) {
+        outdated.style.opacity = opacityValue / 100;
+        outdated.style.filter = 'alpha(opacity=' + opacityValue + ')';
     }
 
-    // function function_fade_out(opacity_value) {
-    //     function_opacity(opacity_value);
-    //     if (opacity_value == 1) {
-    //         outdated.style.display = 'none';
-    //         done = true;
-    //     }
-    // }
-
-    function function_fade_in(opacity_value) {
-        function_opacity(opacity_value);
-        if (opacity_value == 1) {
-            outdated.style.display = 'block';
+    function fadeIn() {
+        function step(opacityValue) {
+            opacity(opacityValue);
         }
-        if (opacity_value == 100) {
-            done = true;
+
+        outdated.style.opacity = 0;
+        outdated.style.display = 'block';
+
+        for (var i = 1; i <= 100; i++) {
+            setTimeout((function (x) {
+                return function () {
+                    step(x);
+                };
+            })(i), i * 8);
         }
     }
-
-    //check if element has a particular class
-    // function hasClass(element, cls) {
-    //     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-    // }
 
     var supports = (function() {
-       var div = document.createElement('div'),
-          vendors = 'Khtml Ms O Moz Webkit'.split(' '),
-          len = vendors.length;
+        var div     = document.createElement('div'),
+            vendors = 'Khtml Ms O Moz Webkit'.split(' '),
+            len     = vendors.length;
 
-       return function(prop) {
-          if ( prop in div.style ) return true;
+        return function(prop) {
+            if (prop in div.style) {
+               return true;
+            }
 
-          prop = prop.replace(/^[a-z]/, function(val) {
-             return val.toUpperCase();
-          });
+            prop = prop.replace(/^[a-z]/, function(val) {
+                return val.toUpperCase();
+            });
 
-          while(len--) {
-             if ( vendors[len] + prop in div.style ) {
-                return true;
-             }
-          }
-          return false;
-       };
+            while (len--) {
+                if (vendors[len] + prop in div.style) {
+                    return true;
+                }
+            }
+
+            return false;
+        };
     })();
 
-    //if browser does not supports css3 property (transform=default), if does > exit all this
-    if ( test || !supports(''+ cssProp +'') ) {
-        if (done && outdated.style.opacity !== '1') {
-            done = false;
-            for (var i = 1; i <= 100; i++) {
-                setTimeout((function (x) {
-                    return function () {
-                        function_fade_in(x);
-                    };
-                })(i), i * 8);
-            }
-        }
-    }else{
+    // if not in test mode and the browser supports css3 property (transform=default) then exit all this
+    if (!options.test && supports(''+ options.lowerThan +'')) {
         return;
-    };//end if
-
-    //Check AJAX Options: if languagePath == '' > use no Ajax way, html is needed inside <div id="outdated">
-    if( languagePath === ' ' || languagePath.length == 0 ){
-        startStylesAndEvents();
-    }else{
-        grabFile(languagePath);
     }
 
-    //events and colors
-    function startStylesAndEvents(){
-        var btnClose = document.getElementById("btnCloseUpdateBrowser");
-        var btnUpdate = document.getElementById("btnUpdateBrowser");
+    if (outdated.style.opacity !== '1') {
+        fadeIn();
+    }
 
-        //check settings attributes
-        outdated.style.backgroundColor = bkgColor;
-        //way too hard to put !important on IE6
-        outdated.style.color = txtColor;
-        outdated.children[0].style.color = txtColor;
-        outdated.children[1].style.color = txtColor;
+    // check AJAX Options: if options.languagePath == '' > use no Ajax way, html is needed inside <div id="outdated">
+    if (options.languagePath === ' ' || options.languagePath.length === 0) {
+        startStylesAndEvents();
+    } else {
+        grabFile(options.languagePath);
+    }
 
-        //check settings attributes
-        btnUpdate.style.color = txtColor;
-        // btnUpdate.style.borderColor = txtColor;
-        if (btnUpdate.style.borderColor) btnUpdate.style.borderColor = txtColor;
-        btnClose.style.color = txtColor;
+    // events and colors
+    function startStylesAndEvents() {
+        var btnClose = document.getElementById("btnCloseUpdateBrowser"),
+            btnUpdate = document.getElementById("btnUpdateBrowser");
 
-        //close button
+        // check settings attributes
+        outdated.style.backgroundColor = options.bgColor;
+        // way too hard to put !important on IE6
+        outdated.style.color = options.color;
+        outdated.children[0].style.color = options.color;
+        outdated.children[1].style.color = options.color;
+
+        // check settings attributes
+        btnUpdate.style.color = options.color;
+        // btnUpdate.style.borderColor = options.color;
+
+        if (btnUpdate.style.borderColor) {
+            btnUpdate.style.borderColor = options.color;
+        }
+
+        btnClose.style.color = options.color;
+
+        // close button
         btnClose.onmousedown = function() {
             outdated.style.display = 'none';
+
             return false;
         };
 
-        //Override the update button color to match the background color
+        // override the update button color to match the background color
         btnUpdate.onmouseover = function() {
-            this.style.color = bkgColor;
-            this.style.backgroundColor = txtColor;
+            this.style.color = options.bgColor;
+            this.style.backgroundColor = options.color;
         };
         btnUpdate.onmouseout = function() {
-            this.style.color = txtColor;
-            this.style.backgroundColor = bkgColor;
+            this.style.color = options.color;
+            this.style.backgroundColor = options.bgColor;
         };
-    }//end styles and events
+    }
 
+    // if AJAX with request ERROR > insert english default
+    var ajaxEnglishDefault = '<h6>Your browser is out-of-date!</h6>' +
+        '<p>Update your browser to view this website correctly. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">Update my browser now </a></p>' +
+        '<p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p>';
 
-    // IF AJAX with request ERROR > insert english default
-    var ajaxEnglishDefault = '<h6>Your browser is out-of-date!</h6>'
-        + '<p>Update your browser to view this website correctly. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">Update my browser now </a></p>'
-        + '<p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p>';
-
-
-    //** AJAX FUNCTIONS - Bulletproof Ajax by Jeremy Keith **
+    /* AJAX FUNCTIONS - Bulletproof Ajax by Jeremy Keith */
     function getHTTPObject() {
-      var xhr = false;
-      if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-      } else if (window.ActiveXObject) {
-        try {
-          xhr = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch(e) {
-          try {
-            xhr = new ActiveXObject("Microsoft.XMLHTTP");
-          } catch(e) {
-            xhr = false;
-          }
+        var xhr = false;
+
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            try {
+                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch(e) {
+                try {
+                    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch(e) {
+                    xhr = false;
+                }
+            }
         }
-      }
-      return xhr;
-    };//end function
+
+        return xhr;
+    }
 
     function grabFile(file) {
         var request = getHTTPObject();
-            if (request) {
-                request.onreadystatechange = function() {
+
+        if (request) {
+            request.onreadystatechange = function() {
                 displayResponse(request);
             };
-                request.open("GET", file, true);
-                request.send(null);
-            }
+
+            request.open("GET", file, true);
+            request.send(null);
+        }
+
         return false;
-    };//end grabFile
+    }
 
     function displayResponse(request) {
-        var insertContentHere = document.getElementById("outdated");
         if (request.readyState == 4) {
             if (request.status == 200 || request.status == 304) {
-                insertContentHere.innerHTML = request.responseText;
-            }else{
-                insertContentHere.innerHTML = ajaxEnglishDefault;
+                outdated.innerHTML = request.responseText;
+            } else {
+                outdated.innerHTML = ajaxEnglishDefault;
             }
+
             startStylesAndEvents();
         }
-      return false;
-    };//end displayResponse
 
-////////END of outdatedBrowser function
+        return false;
+    }
 };
 
 
